7c79cd3021fb638fce6538a1342e6fb43ef700b0
Yuumi <111591095+yuumi4dev@users.noreply.github.com>
2025-02-28 14:48:02 -0600

    firebaseerror

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,17 +1,17 @@
 // Firebase 구성
 const firebaseConfig = {
-    // Firebase 콘솔에서 가져온 구성 정보를 여기에 입력
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-
-// Firebase 초기화
-firebase.initializeApp(firebaseConfig);
-
-// Firebase 서비스 참조
-const auth = firebase.auth();
-const db = firebase.firestore ? firebase.firestore() : null;
+    apiKey: "AIzaSyD2YzgyegEuCZ0ZoP4GEVnNsZMOcqsolrE",
+    authDomain: "githubioproject.firebaseapp.com",
+    projectId: "githubioproject",
+    storageBucket: "githubioproject.appspot.com",
+    messagingSenderId: "8593894041",
+    appId: "1:8593894041:web:e1ec36a1dc5cbad8886608",
+    measurementId: "G-X8CMBLXV52"
+  };
+  
+  // Firebase 초기화
+  firebase.initializeApp(firebaseConfig);
+  
+  // Firebase 서비스 참조
+  const auth = firebase.auth();
+  const db = firebase.firestore ? firebase.firestore() : null;
